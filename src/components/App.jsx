fc025058c173a325d7acda4ca002c2f98e2aa924
Alexander Popov <popov.a.connect@gmail.com>
2023-09-09 15:12:30 +0300

    Corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import React, {useState} from "react";
 
 function App () {
 
+    setInterval(updateTime, 1000);
+
     const now = new Date().toLocaleTimeString();
     const [time, setTime] = useState(now);
 
     function updateTime (){
         const newTime = new Date().toLocaleTimeString();
         setTime(newTime);
-    };
+    }
 
 
     return (
@@ -18,6 +20,6 @@ function App () {
         </div>
       );
     
-};
+}
 
 export default App;
